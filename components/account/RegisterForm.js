ea2ce8f2f181ba4fba2dce99c3725f3844059252
arlestaboada <31904068+arlestaboada@users.noreply.github.com>
2022-09-12 14:48:32 -0500

    Implementar médotos para registrar usuario
     Implementar médotos para registrar usuario

--- components/account/RegisterForm.js
+++ components/account/RegisterForm.js
@@ -1,16 +1,52 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React,{useState} from 'react'
 import {Button,Input,Icon} from "react-native-elements"
+import { validateEmail } from '../../utils/helpers'
+import { size } from 'lodash'
 
 
 export default function RegisterForm() {
   const [showPassword, setShowPassword] = useState(false)
   const [formData, setFormData] = useState(defaultFormValues())
-  
+  const [errorEmail,setErrorEmail]=useState("")
+  const [errorPassword,setErrorPassword]=useState("")
+  const [errorConfirm,setErrorConfirm]=useState("")
   const onChange=(e,type)=>{
     setFormData({...formData,[type]:e.nativeEvent.text})
+    
 
+  }
+  const registerUser=()=>{
+      if(!validateData()){
+        return
+      }
+      console.log("meloncita yay")
 
+  }
+  const validateData=()=>{
+    setErrorConfirm("")
+    setErrorEmail("")
+    setErrorPassword("")
+    let isValid=true
+    if(!validateEmail(formData.email)){
+      setErrorEmail("Debes de ingresar un email válido.")
+      isValid=false
+    }
+    if(size(formData.password)<6){
+      setErrorPassword("Debes ingresar una contraseña de al menos 6 caracteres")
+      isValid=false
+    }
+    if(size(formData.confirm)<6){
+      setErrorConfirm("Debes ingresar una confirmación de al menos 6 caracteres")
+      isValid=false
+    }
+    if(formData.password!==formData.confirm){
+      setErrorPassword("La contraseña y la confirmación no son iguales")
+      setErrorConfirm("La contraseña y la confirmación no son iguales")
+      isValid=false
+
+    }
+      return isValid
   }
   return (
     <View style={styles.form}>
@@ -19,6 +55,8 @@ export default function RegisterForm() {
            placeholder="Ingresa tu email..."
            onChange={(e)=>onChange(e,"email")}
            keyboardType="email-address"
+           errorMessage={errorEmail}
+           defaultValue={formData.email}
         />
         <Input
         containerStyle={styles.input}
@@ -26,6 +64,8 @@ export default function RegisterForm() {
         password={true}
         secureTextEntry={!showPassword}
         onChange={(e)=>onChange(e,"password")}
+        errorMessage={errorPassword}
+        defaultValue={formData.password}
         rightIcon={
           <Icon
           type="material-community"
@@ -44,6 +84,8 @@ export default function RegisterForm() {
         password={true}
         secureTextEntry={!showPassword}
         onChange={(e)=>onChange(e,"confirm")}
+        errorMessage={errorConfirm}
+        defaultValue={formData.confirm}
         rightIcon={
             <Icon
             type="material-community"
@@ -60,7 +102,7 @@ export default function RegisterForm() {
           title="Registrar Nuevo Usuario"
           containerStyle={styles.btnContainer}
           buttonStyle={styles.btn}
-          onPress={()=>console.log(formData)}
+          onPress={()=> registerUser()}
         />
 
     </View>
--- utils/actions.js
+++ utils/actions.js
@@ -2,6 +2,7 @@ import {firebaseApp} from "./firebase"
 import firebase from 'firebase/compat/app'
 import "firebase/firestore"
 
+const db =firebase.firestore(firebaseApp)
 export const isUserLogged=()=>{
     let isLogged=false
     firebase.auth().onAuthStateChanged((user)=>{
@@ -10,6 +11,25 @@ export const isUserLogged=()=>{
 
     return isLogged
 
-
-
 }
+
+export const getCurrentUser=()=>{
+    return firebase.auth().currentUser
+}
+export const closeSession=()=>{
+    return firebase.auth().signOut()
+}
+export const registerUser=async(email,password)=>{
+
+    const result={statusResponse:true,error:null}
+    try {
+        await firebase.auth().createUserWithEmailAndPassword(email,password)
+
+        
+    } catch (error) {
+        result.error="Este correo ya ha sido registrado."
+        
+    }
+}
+
+
