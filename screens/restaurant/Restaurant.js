0e005e76d10c8337faf6439539d0af6eeab09367
arlestaboada <31904068+arlestaboada@users.noreply.github.com>
2022-12-04 18:19:52 -0500

    Colocando el título del restaurante
     Colocando el título del restaurante

--- screens/restaurant/Restaurant.js
+++ screens/restaurant/Restaurant.js
@@ -1,18 +1,106 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React ,{ useLayoutEffect }from 'react'
+import React ,{ useLayoutEffect,useState,useEffect}from 'react'
+import { View } from 'react-native'
+import { Alert,Dimensions ,StyleSheet, Text,  ScrollView} from 'react-native'
+import { Rating } from 'react-native-elements'
+import CarouselImages from '../../components/CarouselImages'
 
+
+import Loading from '../../components/Loading'
+import { getDocumentById } from '../../utils/actions'
+
+const widthScreen=Dimensions.get("window").width
 
 export default function Restaurant({navigation,route}) {
     const {id,name}=route.params
+    const [restaurant, setRestaurant] = useState(null)
+    const [activeSlide, setActiveSlide] = useState(0)
+
     useLayoutEffect(() => {
       navigation.setOptions( { title: name } )
-}, []);
+}, [])
+
+useEffect(() => {
+  (async()=>{
+    const response=await getDocumentById("restaurants",id)
+    if(response.statusResponse){
+      setRestaurant(response.document)
+    }else{
+      setRestaurant({})
+      Alert.alert("Ocurrió un problema cargando el restaurante, intente más tarde.")
+    }
+  })()
+  
+}, [])
+
+if(!restaurant){
+  return <Loading isVisible={true} text="Cargando..."/>
+}
+
    
   return (
-    <View>
-      <Text>Restaurant...</Text>
+    <ScrollView style={styles.viewBody}>
+      <CarouselImages
+       images={restaurant.images}
+       height={250}
+       width={widthScreen}
+       activeSlide={activeSlide}
+       setActiveSlide={setActiveSlide}
+      
+      />
+      <TitleRestaurant
+        name={restaurant.name}
+        description={restaurant.description}
+        rating={restaurant.rating}
+      
+      />
+    </ScrollView>
+  )
+}
+function TitleRestaurant({name,description,rating}){
+
+  return(
+    <View style={styles.viewRestaurantTitle}>
+      <View style={styles.viewRestaurantContainer}>
+        <Text style={styles.nameRestaurant}>{name}</Text>
+        <Rating
+          style={styles.rating}
+          imageSize={20}
+          readonly
+          startingValue={parseFloat(rating)}
+
+        
+        />
+
+      </View>
+      
+      <Text style={styles.descriptionRestaurant}>{description}</Text>
+
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+  viewBody:{
+    flex:1,
+    backgroundColor:"#fff"
+  },
+  viewRestaurantTitle:{
+    padding:15,
+  },
+  viewRestaurantContainer:{
+    flexDirection:"row"
+  },
+  descriptionRestaurant:{
+    marginTop:8,
+    color:"gray",
+    textAlign:"justify"
+  },
+  rating:{
+    position:"absolute",
+    right:0
+  },
+  nameRestaurant:{
+    fontWeight:"bold"
+  }
+})
